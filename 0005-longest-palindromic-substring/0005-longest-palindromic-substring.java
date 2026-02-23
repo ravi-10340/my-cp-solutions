d95047bb895ecb0be1caf2487738ed7e38f23c46
Ravi <ravinarwal3327@gmail.com>
2026-02-23 23:49:27 +0530

    Time: 0 ms (100%), Space: 46.8 MB (37.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -13,18 +13,48 @@ class Solution {
         
         int ans = 0;
         String a = "";
-        for(int i=0;i<s.length();i++){
-            String r = "";
-            for(int j=i;j<s.length();j++){
-                r = s.substring(i,j+1);
-                if(pali(r)){
-                    if(ans<r.length()){
-                        ans = r.length();
-                        a = r;
+        // for(int i=0;i<s.length();i++){
+        //     String r = "";
+        //     for(int j=i;j<s.length();j++){
+        //         r = s.substring(i,j+1);
+        //         if(pali(r)){
+        //             if(ans<r.length()){
+        //                 ans = r.length();
+        //                 a = r;
 
-                    }
-                }
+        //             }
+        //         }
+        //     }
+        // }
+
+        int n = s.length();
+        int i=0;
+        while(i<n){
+            int l = i-1;
+            int r = i+1;
+
+            while((l>=0 && r<n )&& s.charAt(l) == s.charAt(r)){
+                l--;
+                r++;
             }
+            int len = r-l-1;
+            if(ans<len){
+                ans = len;
+                a = s.substring(l+1,r);
+            }
+            l = i;
+            r = i+1;
+
+            while((l>=0 && r<n )&& s.charAt(l) == s.charAt(r)){
+                l--;
+                r++;
+            }
+            len = r-l-1;
+            if(ans<len){
+                ans = len;
+                a = s.substring(l+1,r);
+            }
+            i++;
         }
         return a;
     }
